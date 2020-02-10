4be5ae7aea331da4069029c8b462882d88c96800
chefkasperson <chefkasperson@gmail.com>
2020-02-10 15:31:07 -0800

    Add slug to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,11 @@ class User < ActiveRecord::Base
     message: "only allows a-z, 0-9 and !@#$%^&*()"
   }
 
+  def slug
+    username.downcase.gsub(' ', '-')
+  end
+  
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
 end
