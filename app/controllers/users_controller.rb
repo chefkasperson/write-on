e2c721722047fa613280d706b5a09f16456e2a35
chefkasperson <chefkasperson@gmail.com>
2020-02-10 12:03:20 -0800

    added view files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
-  
+  get 'users' do
+    erb :'/users/index'
+  end
+
+  get '/signup' do
+    erb :'users/new'
+  end
+
+  post '/signup' do
+    binding.pry
+  end
 end
