53633a31c10c7c652a09f00ecc1d2cb030b41843
chefkasperson <chefkasperson@gmail.com>
2020-02-10 10:12:40 -0800

    Added Validations to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :username, length: {
+    minimum: 6,
+    maximum: 35,
+  }, uniqueness: true
+
+  validates :password, format: {
+    with: /\A[a-zA-Z0-9!@#\$%^&\(\)]+\z/,
+    message: "only allows a-z, 0-9 and !@#$%^&*()"
+  }
+
 end
