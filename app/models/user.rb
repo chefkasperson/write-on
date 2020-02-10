6722215623468d08acbf456bce6bac91013ca69b
chefkasperson <chefkasperson@gmail.com>
2020-02-10 14:53:56 -0800

    Fix Password Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,6 @@ class User < ActiveRecord::Base
   validates :password, format: {
     with: /\A[a-zA-Z0-9!@#\$%^&\(\)]+\z/,
     message: "only allows a-z, 0-9 and !@#$%^&*()"
-  }, length: {
-    minimum: 6,
-    maximum: 50
-  }, uniqueness: true
-  
+  }
 
 end
