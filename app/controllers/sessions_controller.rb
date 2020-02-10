cbae7bc8f4d098bb7939cecab3eb4eabbc4275ca
chefkasperson <chefkasperson@gmail.com>
2020-02-10 11:33:21 -0800

    Added Login and Logout Routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  get '/login' do
+    erb :'sessions/new'
+  end
+
+  post 'login' do
+    user = User.find_by(email: params[:user][:email])
+
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect to "/users/#{user.id}"
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/logout' do
+    session.clear
+    redirect to '/'
+  end
 end
