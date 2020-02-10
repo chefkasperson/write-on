e157e2b728f1562ce67a8ccdc7a0c200f8d28d41
chefkasperson <chefkasperson@gmail.com>
2020-02-10 10:15:21 -0800

    Added Content to Stories Migration

--- db/migrate/20200210173218_create_stories.rb
+++ db/migrate/20200210173218_create_stories.rb
@@ -2,6 +2,7 @@ class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
       t.string :title
+      t.text :content
       t.integer :average_rating
       t.boolean :published?, default: false
       t.integer :user_id
