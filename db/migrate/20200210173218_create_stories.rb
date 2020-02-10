9e983ab11c56674492548081cea5c8c4f200811c
chefkasperson <chefkasperson@gmail.com>
2020-02-10 10:07:53 -0800

    Updated Migration Files

--- db/migrate/20200210173218_create_stories.rb
+++ db/migrate/20200210173218_create_stories.rb
@@ -1,4 +1,13 @@
 class CreateStories < ActiveRecord::Migration
   def change
+    create_table :stories do |t|
+      t.string :title
+      t.integer :average_rating
+      t.boolean :published?, default: false
+      t.integer :user_id
+      t.integer :jam_id
+
+      t.timestamps null: false
+    end
   end
 end
--- db/migrate/20200210173354_create_jams.rb
+++ db/migrate/20200210173354_create_jams.rb
@@ -1,4 +1,13 @@
 class CreateJams < ActiveRecord::Migration
   def change
+    create_table :jams do |t|
+      t.string :name
+      t.string :theme
+      t.string :status, default: 'upcoming'
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps null: false
+    end
   end
 end
