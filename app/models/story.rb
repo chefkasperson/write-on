330e49bb46144dc1df704c562f8471454695359c
chefkasperson <chefkasperson@gmail.com>
2020-02-10 15:43:32 -0800

    Add slug to story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,12 @@
 class Story < ActiveRecord::Base
   belongs_to :user
   belongs_to :jam
+
+  def slug
+    title.downcase.gsub(' ', '-')
+  end
+
+  def find_by_slug(slug)
+    Story.all.find{|story| story.slug == slug}
+  end
 end
