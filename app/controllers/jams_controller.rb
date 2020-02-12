88287d50e4a6a53f0be663197d387842f054c4aa
chefkasperson <chefkasperson@gmail.com>
2020-02-12 02:17:43 -0800

    Add Post Route

--- app/controllers/jams_controller.rb
+++ app/controllers/jams_controller.rb
@@ -15,9 +15,19 @@ class JamsController < ApplicationController
 
   post '/jams' do
     if logged_in? && current_user.permission == 'admin'
-      if 
+      if params[:title] == "" || params[:start_date] == "" || params[:end_date] == ""
+        redirect to '/jams/new'
+      else
+        @jam = Jam.new(params)
+        if @jam.save
+          redirect to '/jams/#{@jam.id}'
+        else
+          redirect to '/jams/new'
+        end
+      end
     else
       redirect to '/jams'
     end
+  end
 end
 
