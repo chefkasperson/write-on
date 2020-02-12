1efa32ea938a968d3307809b0e6252e66416fffa
chefkasperson <chefkasperson@gmail.com>
2020-02-12 03:38:44 -0800

    Add story separation method

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,11 +2,8 @@ class Story < ActiveRecord::Base
   belongs_to :user
   belongs_to :jam
 
-  def slug
-    title.downcase.gsub(' ', '-')
+  def published_stories
+    Story.all.select{|story| story.published? == true}
   end
 
-  def find_by_slug(slug)
-    Story.all.find{|story| story.slug == slug}
-  end
 end
