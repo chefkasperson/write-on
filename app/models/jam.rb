20e93923797758b6704dad97d902fa10063bc466
chefkasperson <chefkasperson@gmail.com>
2020-02-12 03:38:29 -0800

    Add Jam Separation methods

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -1,4 +1,16 @@
 class Jam < ActiveRecord::Base
   has_many :stories
   has_many :users, through: :stories
+
+  def open_jams
+    Jam.all.select{|jam| jam.status == 'open'}
+  end
+
+  def voting_jams
+    Jam.all.select{|jam| jam.status == 'voting'}
+  end
+
+  def closed_jams
+    Jam.all.select{|jam| jam.status == 'closed'}
+  end
 end
